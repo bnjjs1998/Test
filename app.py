8774c67b4cae1f64a421785ced90e82a8d640ec8
bnjjs <baptist1998.jaques@gmail.com>
2024-12-28 13:23:50 +0100

    Paufiner le service

--- app.py
+++ app.py
@@ -1,11 +1,55 @@
-from flask import Flask, render_template, redirect, url_for, request
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import Flask, request, render_template, redirect, url_for, session
+from pymongo import MongoClient
 
 app = Flask(__name__)
-#je vais configurer mongo
-from model import *
-from route import *
+app.secret_key = "your_secret_key"  # Remplacez par une clé sécurisée
+
+# Connexion à MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client.mydb
+users_collection = db["User"]
 
 
-if __name__ == '__main__':
+@app.route("/")
+def home():
+    if "username" in session:
+        return f"Bienvenue, {session['username']}! <a href='/logout'>Déconnexion</a>"
+    return "Vous n'êtes pas connecté. <a href='/login'>Connexion</a> ou <a href='/register'>Inscription</a>"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = users_collection.find_one({"username": username, "password": password})
+        if user:
+            session["username"] = username
+            return redirect(url_for("home"))
+        return "Nom d'utilisateur ou mot de passe incorrect."
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if users_collection.find_one({"username": username}):
+            return "Nom d'utilisateur déjà pris."
+
+        users_collection.insert_one({"username": username, "password": password})
+        return redirect(url_for("login"))
+    return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("home"))
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- configMongo.py
+++
@@ -1,9 +0,0 @@
-#Le fichier se nomme configMongo.py
-from app import app
-from flask_pymongo import PyMongo
-
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mydb"
-mongo = PyMongo(app)
-
-
-
